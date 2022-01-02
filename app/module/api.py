435d2dbf9f3b6087aa38d2b97b9f21167ed1fccc
judanticahyaning <judantityas@gmail.com>
2022-01-02 21:16:50 +0700

    last commit

--- app/module/api.py
+++ app/module/api.py
@@ -44,39 +44,25 @@ class add(Resource):
         response = {"msg": "Berhasil!"}
         return response
 
-# class edit(Resource):
-#     def put(self,id):
-#         # id = request.form['id']
-#         title = request.form['title']
-#         genre = request.form['genre']
-#         rating = request.form['rating']
-#         duration = request.form['duration']
-#         quality = request.form['quality']
-#         trailer = request.form['trailer']
-#         watch = request.form['watch']
-#         sql = engine.execute("UPDATE film SET title=%s, genre=%s, rating=%s, duration=%s, quality=%s, "
-#                              "trailer=%s, watch=%s WHERE id=%s",
-#                              (title, genre, rating, duration, quality, trailer, watch, id))
-#         output = [{
-#                      "id":data.id,
-#                      "title":data.title,
-#                      "genre":data.genre,
-#                      "rating":data.rating,
-#                      "duration":data.duration,
-#                      "quality":data.quality,
-#                      "trailer":data.trailer,
-#                      "watch":data.watch
-#                  }
-#                      for data in sql
-#                  ]
-#         response ={
-#              "msg" : "berhasil ditampilkan",
-#              "data" : output
-#
-#         }
-#         return response
+class edit(Resource):
+    def put(self,id):
+        # id = request.form['id']
+        title = request.form['title']
+        genre = request.form['genre']
+        rating = request.form['rating']
+        duration = request.form['duration']
+        quality = request.form['quality']
+        trailer = request.form['trailer']
+        watch = request.form['watch']
+        sql = engine.execute("UPDATE film SET title=%s, genre=%s, rating=%s, duration=%s, quality=%s, "
+                             "trailer=%s, watch=%s WHERE id=%s",
+                             (title, genre, rating, duration, quality, trailer, watch, id))
+        response ={
+             "msg" : "berhasil diupdate"
+        }
+        return response
 
-# class delete(Resource):
+# class delfilm(Resource):
 #     def hapus(self, id):
 #         sql = engine.execute("DELETE FROM film WHERE id=" + id)
 #         response = {"msg":"berhasil dihapus"}
@@ -84,6 +70,6 @@ class add(Resource):
 
 api.add_resource(show, "/data", methods=["GET"])
 api.add_resource(add, "/addFilm", methods=["POST"])
-# api.add_resource(edit, "/editFilm/<id>", methods=["PUT"])
+api.add_resource(edit, "/editFilm/<id>", methods=["PUT"])
 # yang belum bisa delete
-# api.add_resource(delete, "/delFilm/<id>", methods=["DELETE"])
+# api.add_resource(delfilm, "/delFilm/<id>", methods=["DELETE"])
--- app/module/controller.py
+++ app/module/controller.py
@@ -3,7 +3,6 @@ from flask import render_template, url_for, request, redirect, make_response
 from app.module.api import *
 from sqlalchemy import create_engine
 from flask import jsonify
-
 from app.module.api import api
 
 engine = create_engine("mysql+mysqlconnector://root@localhost:3306/knowledge_test", echo=False)
@@ -14,35 +13,12 @@ def index():
 
 @app.route('/addFilm', methods=['POST'])
 def addFilm():
-    # if request.method == "POST":
-    #     api.add_resource(add, "/addFilms", methods=["POST"])
-    # title = request.form['title']
-    # genre = request.form['genre']
-    # rating = request.form['rating']
-    # duration = request.form['duration']
-    # quality = request.form['quality']
-    # trailer = request.form['trailer']
-    # watch = request.form['watch']
-    #
-    # sql = "INSERT INTO film(title, genre, rating, duration, quality, trailer, watch) VALUES (%s, %s, %s, %s,%s,%s,%s)"
-    # data = (title, genre, rating, duration, quality, trailer, watch)
-    # proses = engine.execute(sql, data)
+
     return redirect(url_for('index'))
 
 @app.route('/editFilm/<id>', methods=['PUT'])
 def editFilm(id):
-    title = request.form['title']
-    genre = request.form['genre']
-    rating = request.form['rating']
-    duration = request.form['duration']
-    quality = request.form['quality']
-    trailer = request.form['trailer']
-    watch = request.form['watch']
-    sql = engine.execute("UPDATE film SET title=%s, genre=%s, rating=%s, duration=%s, quality=%s, "
-                         "trailer=%s, watch=%s WHERE id=%s",
-                         (title, genre, rating, duration, quality, trailer, watch, id))
-    # api.add_resource(edit, "/editFilm/<id>", methods=["PUT"])
-    # return redirect(url_for('index'))
+
     return jsonify("berhasil")
 
 @app.route('/delFilm/<id>',methods=['DELETE'])
